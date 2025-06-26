6c73632a0a8737da1e7bb050407bbe4f6add10ca
Asser Osama <asser.usama@decathlon.com>
2025-06-26 23:26:17 +0300

    🎉 initial commit
    🎉 initial commit
    :wq
    :wq!

---
+++ Ganzabel/GanzabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  GanzabelApp.swift
+//  Ganzabel
+//
+//  Created by Asser on 26/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct GanzabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+
+        }
+    }
+}
